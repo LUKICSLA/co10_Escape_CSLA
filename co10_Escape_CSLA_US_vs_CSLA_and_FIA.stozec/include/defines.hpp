0885999d304c7c3d6954c7f362ac159defabb906
Lukáš Maár <luki.m2k@gmail.com>
2022-12-03 12:31:24 +0100

    init

---
+++ co10_Escape_CSLA_US_vs_CSLA_and_FIA.stozec/include/defines.hpp
@@ -0,0 +1,18 @@
+#define VERSION "1.10 dev 221203 1149"
+#define MISSIONNAME "co10 Escape"
+#define MISSIONNAMEFULL "co10 Escape Gabreta CSLA US vs CSLA+FIA 1.10 dev 221203 1149"
+#define MOD "CSLA US vs CSLA+FIA"
+#define ISLAND "Gabreta"
+#define RELEASE "CSLA Studio"
+
+//--- [Controls Style Preprocessors]
+#define ST_LEFT 				0
+#define ST_RIGHT 				1
+#define ST_CENTER 				2
+#define ST_MULTI 				16
+#define ST_PICTURE 				48
+#define ST_TEXT_BG 				128
+#define ST_LINE 				176
+#define ST_KEEP_ASPECT_RATIO	"0x30 + 0x800"
+#define CT_STRUCTURED_TEXT      13
+#define LB_MULTI 				0x20
